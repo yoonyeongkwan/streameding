d95b2da2ebc77d28b1ec6c990ded7ada503422d0
ura <guddk3434@gmail.com>
2024-08-22 15:21:23 +0900

    스트리미딩 생성

---
+++ Server/streameding/src/main/java/streaming/streameding/StreamedingApplication.java
@@ -0,0 +1,13 @@
+package streaming.streameding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamedingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamedingApplication.class, args);
+	}
+
+}
---
+++ Server/streameding/src/test/java/streaming/streameding/StreamedingApplicationTests.java
@@ -0,0 +1,13 @@
+package streaming.streameding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamedingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
